bae680c98dfe71d12910ab029ec140c776b36b9b
dacucar <dario.cut@gmail.com>
2016-05-22 16:44:12 +0200

    Add Draft font creator

---
+++ FenixLib/Examples/FontConverter/MainWindow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{
+    public MainWindow ()
+        : base ( Gtk.WindowType.Toplevel )
+    {
+        Build ();
+        textview1.Buffer.Changed += new EventHandler ( OnTextChanged );
+    }
+
+    protected void OnDeleteEvent ( object sender, DeleteEventArgs a )
+    {
+        Application.Quit ();
+        a.RetVal = true;
+    }
+
+    protected void OnTextChanged (object o, EventArgs args)
+    {
+        // var dlg = new MessageDialog ( this, DialogFlags.Modal, MessageType.Info, ButtonsType.Ok, "Lala" );
+        // dlg.Run ();
+        // dlg.Destroy ();
+    }
+}
